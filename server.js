060e46d4a7c575ea26e86bba9f948cf519080e53
Rohan Unbeg <rohanunbeg0918@gmail.com>
2024-08-27 20:49:31 +0530

    seting up firebase

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(express.static(path.join(__dirname, "public"))); // Serves static files 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
 
 app.post("/generate-code", async (req, res) => {
   const { prompt } = req.body;
